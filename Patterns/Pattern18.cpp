5be55dcd1f6682e5c7e430681f539050146515e6
Soubhik <soubhik285@gmail.com>
2023-01-08 15:31:33 +0530

    explanation updated

--- Patterns/Pattern18.cpp
+++ Patterns/Pattern18.cpp
@@ -9,7 +9,7 @@
 // No of rows = "n". Thus, run a loop for n times(outer loop)
 // alphabets (A,B,C,D...etc) so we can use ASCII code for that (A starts with ASCII code 65)
 // In every row, total elements printed = row'th no (i.e in 1st row, there is one element printed, in 2nd row 2 elements are printed, etc)
-// Also, every element starts from 65 + (n-i)th ASCII character and prints all remaining characters by -1 ASCII. ( i is row no)
+// Also, every element starts from 65 + (n-1)th ASCII character and prints all subsequent characters upto 65 + (n-i)th ASCII (where i is row no)
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -18,7 +18,7 @@ int main()
     int n = 5;
     for (int i = 1; i <=n; i++)
     {
-        for (int j = n - i; j < n; j++)
+        for (int j = n - 1; j >= n - i; j--)
         {
             cout << char(j + 65) << " ";
         }
