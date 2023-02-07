cdabeb3281549ad3186be98bba890de2b22a0173
Soubhik <soubhik285@gmail.com>
2023-02-08 02:31:37 +0530

    Explanation updated

--- General Questions/kadaneAlgo.cpp
+++ General Questions/kadaneAlgo.cpp
@@ -1,31 +1,36 @@
 /*Questions :- Write a efficient program to find the max sum of a contiguous subarray within a one-dimensional array of numbers that has the largest sum.*/
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int maxSubArraySum(int a[],int n)
+/*logic :- 1. Update max_sum only when we achieve a greater value of max_ends
+           2. Unless until max_ends < 0, keep adding the array elements until the end of array.
+           3. If at any point, it < 0 then make max_sum = 0 and repeat
+           */
+
+
+int maxSubArraySum(int a[], int n)
 {
-    int max_sum=INT_MIN, max_ends=0;
-    for(int i=0;i<n;i++)
-        {
-            max_ends = max_ends + a[i];
-            if(max_ends> max_sum)                               //kadane algo
-                max_sum = max_ends;
-            if(max_ends<0)
-                max_ends=0;
-        }
+    int max_sum = INT_MIN, max_ends = 0;
+    for (int i = 0; i < n; i++)
+    {
+        max_ends = max_ends + a[i];
+        if (max_ends > max_sum) // kadane algo
+            max_sum = max_ends;
+        if (max_ends < 0)
+            max_ends = 0;
+    }
     return max_sum;
 }
 
 int main()
 {
     int a[] = {-2, -3, 4, -1, -4, 1, 5, -3};
-    int n = sizeof(a)/sizeof(a[0]);
+    int n = sizeof(a) / sizeof(a[0]);
     int max_sum = maxSubArraySum(a, n);
     cout << "Maximum contiguous sum is " << max_sum;
     return 0;
 }
 
-
 //@Soubhik_Ghosh
-//TC -> o(n)
-//SC -> o(1)
+// TC -> o(n)
+// SC -> o(1)
