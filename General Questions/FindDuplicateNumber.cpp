e08f7732c36b0da2afbb7900bb0e931c5ef6b354
Soubhik <soubhik285@gmail.com>
2023-02-10 03:04:50 +0530

    code quality improved

--- General Questions/FindDuplicateNumber.cpp
+++ General Questions/FindDuplicateNumber.cpp
@@ -1,28 +1,36 @@
 /*Question :- Find the duplicate number in an array of sizr N+1*/
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
-int findDuplicate(vector < int > & nums) {
+
+// Used logic of find the starting point of a cycle in linked list
+// here instead of creating a linked list, we used indexes as numbers are in range 0 to n.
+int findDuplicate(vector<int> &nums)
+{
   int slow = nums[0];
   int fast = nums[0];
-  do {
+  
+  do
+  {
     slow = nums[slow];
     fast = nums[nums[fast]];
   } while (slow != fast);
+
   fast = nums[0];
-  while (slow != fast) {
+  while (slow != fast)
+  {
     slow = nums[slow];
     fast = nums[fast];
   }
+
   return slow;
 }
-int main() {
-  vector < int > arr;
-  arr = {1,3,4,2,3};
+int main()
+{
+  vector<int> arr;
+  arr = {1, 3, 4, 2, 3};
   cout << "The duplicate element is " << findDuplicate(arr) << endl;
 }
 
-//@Soubhik_Ghosh
 
-//TC -> o(n)
-//SC -> o(1)
+// TC -> o(n)
+// SC -> o(1)
