397f5ae61a196309858edf85b19975a7a1ff641e
Soubhik <soubhik285@gmail.com>
2023-02-10 02:24:07 +0530

    explanation updated

--- General Questions/2sum.cpp
+++ General Questions/2sum.cpp
@@ -3,35 +3,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function that return answer in yes or no
+// Brute Force:- TC -> O(N^2), SC -> O(1)
+// Better Way, Sort and then 2 pointer approach -> TC -> O(NlogN), SC -> O(N) 
+
+// Optimal Approach :- Using hash map
 int sumexists(int arr[], int n, int k)
 {
     unordered_set<int> m;
-    for (int i =0; i<n ; i++)
+    for (int i = 0; i < n; i++)
     {
-        if(m.find(k-arr[i])!=m.end())
+        if (m.find(k - arr[i]) != m.end())
         {
-            cout<<"Pair exists "<<"("<<arr[i]<<","<<k-arr[i]<<")";
+            cout << "Pair exists "
+                 << "(" << arr[i] << "," << k - arr[i] << ")";
             return 1;
         }
-            
-        m.insert(arr[i]);       
+
+        m.insert(arr[i]);
     }
-    cout<<"Pair Doesnot exist";
+    cout << "Pair Doesnot exist";
     return 0;
-
-
-
 }
+
 
 // Driver Function
 int main()
 {
-	int arr[] = { 1,4,2,7,5,3,9,6};
-	int N = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {1, 4, 2, 7, 5, 3, 9, 6};
+    int N = sizeof(arr) / sizeof(arr[0]);
     int target = 150;
     sumexists(arr, N, target);
-	return 0;
+    return 0;
 }
 
-//@Soubhik_Ghosh
+//TC -> O(N)
+//SC -> O(N)
--- General Questions/Reversepairs.cpp
+++ General Questions/Reversepairs.cpp
@@ -2,74 +2,91 @@
 
 A reverse pair is a pair (i, j) where 0 <= i < j < nums.length and nums[i] > 2 * nums[j].*/
 
-
 #include <bits/stdc++.h>
 using namespace std;
 
 // Function that counts number of reverse pair
-class Solution {
+class Solution
+{
 public:
-    
-    int merge(vector<int>& nums, int l, int mid, int h) {
-        
+    int merge(vector<int> &nums, int l, int mid, int h)
+    {
+
         int total = 0;
-        int j = mid+1 ;
-        for(int i = l; i<=mid; i++) {
-            while(j<=h && nums[i] > 2ll*nums[j]) {
+        int j = mid + 1;
+        //find reverse pairs
+        for (int i = l; i <= mid; i++)
+        {
+            while (j <= h && nums[i] > 2 * nums[j])
+            {
                 j++;
             }
-            total += (j-(mid+1));
+
+            total += (j - (mid + 1));
         }
+
+        //merge sort remaining part
         vector<int> temp;
-        int left = l, right = mid+1;
-        while(left<=mid && right<=h) {
-            if(nums[left] <= nums[right]) {
+        int left = l, right = mid + 1;
+        while (left <= mid && right <= h)
+        {
+            if (nums[left] <= nums[right])
+            {
                 temp.push_back(nums[left++]);
-            } else {
+            }
+            else
+            {
                 temp.push_back(nums[right++]);
             }
         }
-        while(left<=mid) {
+
+        
+        while (left <= mid)
+        {
             temp.push_back(nums[left++]);
         }
-        while(right<=h) {
+        while (right <= h)
+        {
             temp.push_back(nums[right++]);
         }
-        for(int i=l; i<=h; i++) {
-            nums[i] = temp[i-l];
+
+        for (int i = l; i <= h; i++)
+        {
+            nums[i] = temp[i - l];
         }
+
         return total;
     }
-    
-    int mergeSort(vector<int>& nums, int l, int h) {
-        
-        if(l >= h) return 0;
-        int mid = (l+h)/2;
+
+    int mergeSort(vector<int> &nums, int l, int h)
+    {
+
+        if (l >= h)
+            return 0;
+        int mid = (l + h) / 2;
         int p = mergeSort(nums, l, mid);
-        p += mergeSort(nums, mid+1, h);
+        p += mergeSort(nums, mid + 1, h);
         p += merge(nums, l, mid, h);
         return p;
-        
-    } 
-    
-    int reversePairs(vector<int>& nums) {
-        
+    }
+
+    int reversePairs(vector<int> &nums)
+    {
+
         int n = nums.size();
-        return mergeSort(nums, 0, n-1);
-        
+        return mergeSort(nums, 0, n - 1);
     }
 };
 
 // Driver Function
 int main()
 {
-	vector<int> arr ={3,2,5,4,1};
+    vector<int> arr = {3, 2, 5, 4, 1};
     Solution s1;
-    int ans=s1.reversePairs(arr);
-    cout<<"Number of Reverse pairs in this array :- "<<ans; 
-	return 0;
+    int ans = s1.reversePairs(arr);
+    cout << "Number of Reverse pairs in this array :- " << ans;
+    return 0;
 }
 
-//@Soubhik_Ghosh
 // TC -> o(nlogn)+o(n)+o(n)
 // SC -> o(n)
