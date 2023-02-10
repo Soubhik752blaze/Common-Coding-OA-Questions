3210d0a09693d9b620031a566ce6e28ddb824762
Soubhik <soubhik285@gmail.com>
2023-02-11 03:03:30 +0530

    code quality improved

--- General Questions/largestsubarraywith0sum.cpp
+++ General Questions/largestsubarraywith0sum.cpp
@@ -1,39 +1,36 @@
 /*Questions :- Write a efficient program to find the length of the max contiguous subarray with sum 0 within a one-dimensional array of numbers that has the largest sum.*/
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int maxSubArraySum(int arr[],int n)
+//same problem as largest subarray with sum K -> check that out in same repository
+int maxSubArraySum(int arr[], int n)
 {
-    unordered_map<int,int> mp;
+    unordered_map<int, int> mp;
     int maxi = INT_MIN;
-    int sum=0;
-    for(int i=0;i<n;i++)
+    int sum = 0;
+    for (int i = 0; i < n; i++)
     {
-        sum+=arr[i];
-        if(sum==0)
-            maxi = i+1;                 //means from starting upto i thats the longest subarray with sum 0
-        if(mp.find(sum)!=mp.end())
-            maxi=max(maxi,i-mp[sum]);
+        sum += arr[i];
+        if (sum == 0)
+            maxi = i + 1; // means from starting upto i thats the longest subarray with sum 0
+        if (mp.find(sum) != mp.end())
+            maxi = max(maxi, i - mp[sum]);
         else
-            mp[sum]=i;       
-
+            mp[sum] = i;
     }
     return maxi;
-
 }
 
 int main()
 {
     int a[] = {2, -3, 4, -1, -4, 1, 5, -3};
-    int n = sizeof(a)/sizeof(a[0]);
+    int n = sizeof(a) / sizeof(a[0]);
     int maxlength = maxSubArraySum(a, n);
     cout << "Length of max contiguous array with sum 0 is :- " << maxlength;
     return 0;
 }
 
+// TC -> o(N)
+// SC -> o(N)
 
-//@Soubhik_Ghosh
-//TC -> o(N)
-//SC -> o(N)
-
-//BF -> o(N^2)
+// BF -> o(N^2)
