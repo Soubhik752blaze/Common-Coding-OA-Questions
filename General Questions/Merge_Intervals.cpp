514006b8231d64b25de48fa88156a6c11f48cf7b
Soubhik <soubhik285@gmail.com>
2023-02-11 03:02:06 +0530

    code quality and explanation improved

--- General Questions/Merge_Intervals.cpp
+++ General Questions/Merge_Intervals.cpp
@@ -10,30 +10,33 @@ vector<vector<int>> merge(vector<vector<int>> &intervals)
     vector<vector<int>> ans;
     if (intervals.size() == 0)
         return ans;
-    
-    //sorting on basis of starting points
-    sort(intervals.begin(), intervals.end());
 
+    // sorting on basis of starting points
+    sort(intervals.begin(), intervals.end());
 
     vector<int> tempmerged = intervals[0];
     for (auto it : intervals)
     {
+        // if found a overlapping interval, have its ending point as the max of the 2 end points
         if (it[0] <= tempmerged[1])
             tempmerged[1] = max(tempmerged[1], it[1]);
         else
         {
-            merged.push_back(tempmerged);
+            // if found a non-overlapping interval, push the earlier interval to ans
+            //  then assign tempmerged as this interval for further analysis
+            ans.push_back(tempmerged);
             tempmerged = it;
         }
     }
 
+    //push last interval into merged as it was analysed but never got chance to be pushed into ans at the end
     ans.push_back(tempmerged);
     return ans;
 }
 
 int main()
 {
-    vector<vector<int>> nums = {{1, 6}, {2, 8}, {10, 15}, {17, 20}};
+    vector<vector<int>> nums = {{1, 6}, {2, 8}, {17, 20}, {10, 15}};
     vector<vector<int>> ans = merge(nums);
     for (auto it : ans)
     {
